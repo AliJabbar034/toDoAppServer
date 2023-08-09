d505bedfd831685e8c97112800cd762aa4ee3d3e
AliJabbar034 <alijabbar0034@gmail.com>
2023-08-09 13:34:42 +0500

    final

--- app.js
+++ app.js
@@ -3,17 +3,33 @@ import userRouter from './routers/userRoute.js'
 import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
- export const app = express();
+import { config } from "dotenv";
+
+import { connectDatabase } from "./config/database.js";
+
+
+const app = express();
+ 
+const port =process.env.PORT || 5000;
  app.use(express.json());
  app.use(cookieParser());
  app.use(express.urlencoded({ extended: true }))
+ app.use('/api/v1',userRouter); 
 app.use(fileUpload({
     limits:{ fileSize : 50 *1024 * 1024},
     useTempFiles:true
 }))
+config({
+    path:'./config/config.env'
+})
 
 app.use(cors())
 app.use(cors({
     origin: '*'
 }));
- app.use('/api/v1',userRouter); 
+ 
+
+ await connectDatabase();
+app.listen(port, ()=>{
+    console.log("Server running on port " + port);
+})
--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-import { config } from "dotenv";
-import { app } from "./app.js";
-import { connectDatabase } from "./config/database.js";
-import cloudinary from 'cloudinary'
+// import { config } from "dotenv";
+// import { app } from "./app.js";
+// import { connectDatabase } from "./config/database.js";
+// import cloudinary from 'cloudinary'
 
-const port =process.env.PORT || 5000;
-config({
-    path:'./config/config.env'
-})
+// const port =process.env.PORT || 5000;
+// config({
+//     path:'./config/config.env'
+// })
 
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.API_KEY,
-    api_secret: process.env.API_SECRET
-})
-await connectDatabase();
-app.listen(port, ()=>{
-    console.log("Server running on port " + port);
-})
+// cloudinary.config({
+//     cloud_name: process.env.CLOUD_NAME,
+//     api_key: process.env.API_KEY,
+//     api_secret: process.env.API_SECRET
+// })
+// await connectDatabase();
+// app.listen(port, ()=>{
+//     console.log("Server running on port " + port);
+// })
