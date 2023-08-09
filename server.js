f540c570bc8526298a8eb2f43e82c15c42d1fbba
AliJabbar034 <136952448+AliJabbar034@users.noreply.github.com>
2023-08-09 09:59:24 +0500

    Update server.js

--- server.js
+++ server.js
@@ -14,6 +14,6 @@ cloudinary.config({
 })
 
 await connectDatabase();
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT || 5000, ()=>{
     console.log("Server running on port " + process.env.PORT);
 })
